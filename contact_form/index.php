bf876dabff0d72e741352f5c0f39fbd419c7f5c0
djreimer <djreimer@a0278ad8-05c7-11de-b17c-0f1ef23b492c>
2009-02-28 19:57:21 +0000

    Initial commit of contact_form

---
+++ contact_form/index.php
@@ -0,0 +1,16 @@
+<?php/* * Contact Form - Basic contact form for Frog CMS * * Original Author: A.J. Cates; http://ajcates.com/ * Updated by: D.J. Reimer; http://erajad.byethost10.com/ * * Licensed under the MIT license: *   http://www.opensource.org/licenses/mit-license.php * */
+Plugin::setInfos(array(
+    'id'          => 'contact_form',    'title'       => 'Contact Form',     'description' => 'A contact form plugin.',     'version'     => '1.0.1',     'license'     => 'MIT',	'require_frog_version' => '0.9.2',    'website'     => 'http://ajcates.com/'));
+function makeForm($emailOut) {
+if(isset($_POST["name"],$_POST["email"], $_POST["content"])) {
+	$to = $emailOut;
+//	$subject = "Contact form from " . $_POST["name"];
+	$subject = $_POST["name"];	$message = $_POST["content"];	$from = $_POST["email"];	$headers = "From: $from";
+	    if(empty($subject) || empty($from) || empty($message )) {            echo "<p><strong>Reload About page - fill in all fields!</strong></p>";            die();        }
+	mail($to,$subject,$message,$headers);	echo "<p><em>Message sent!</em></p><p>Please allow up to three days for a reply.</p>";
+    } else {
+	echo <<<END
+<div class="contactForm"><form id="gb_form" method="post" action="#"><p>Name:<br /><input type="text" name="name"/></p><p>Email:<br /><input type="text" name="email"/></p><p>Message:<br /><textarea name="content" rows="10" cols="45" wrap="physical"></textarea></p><p><input class="submit" type="submit" value="Send"/></p></form></div>
+END;
+    }
+}?>
