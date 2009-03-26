c0df6b91c3c0a26bc87c036b330052a0474035e3
djreimer <djreimer@a0278ad8-05c7-11de-b17c-0f1ef23b492c>
2009-03-26 17:18:53 +0000

    Adding French language file.

---
+++ trunk/contact_form/i18n/fr-message.php
@@ -0,0 +1,22 @@
+<?php
+
+    /**
+     * French file for plugin contact_form
+     *
+     * @package frog
+     * @subpackage plugin.contact_form.translations
+     *
+     * @author Google Language Tools :)
+     * @version Frog 0.9.4+
+     */
+
+    return array(
+    'Email:' => 'Email:',
+	'Ensure all fields are complete, or you will have to start again!' => 'Veiller à ce que tous les champs soient complètes, ou vous devrez recommencer!',
+	'Message sent!' => 'Message envoyé!',
+	'Message:' => 'Message:',
+	'Name:' => 'Nom:',
+	'No thanks!' => 'Non, merci!',
+	'Please allow up to three days for a reply.' => 'S’il vous plaît permettre jusqu’à trois jours pour la réponse.',
+	'Reload this page - fill in all fields!' => 'Recharger cette page - remplir toutes les cases!',
+    );
